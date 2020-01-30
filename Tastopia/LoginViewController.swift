23f0e5aac01b756e7436852a2b031e4a9e34ddcc
Bacteriafish <you@example.com>
2020-01-30 15:53:31 +0800

    add UIColor extension: hex to rgb add color: SUMI, HAI

--- Tastopia/LoginViewController.swift
+++ Tastopia/LoginViewController.swift
@@ -18,16 +18,24 @@ class LoginViewController: UIViewController {
         
         GIDSignIn.sharedInstance()?.presentingViewController = self
         
-//        Auth.auth().addStateDidChangeListener { (auth, user) in
-//
-//        }
+        //        Auth.auth().addStateDidChangeListener { (auth, user) in
+        //
+        //        }
     }
-
-    @IBAction func googleSignIn(_ sender: Any) {
+    
+    @IBAction func googleSignInPress(_ sender: Any) {
+        googleSignIn()
+    }
+    
+    func googleSignIn() {
         GIDSignIn.sharedInstance().signIn()
     }
     
-    @IBAction func facebookLogin(_ sender: Any) {
+    @IBAction func fbLogin(_ sender: Any) {
+        facebookLogin()
+    }
+    
+    func facebookLogin() {
         let loginManager = LoginManager()
         loginManager.logIn(
             permissions: [.publicProfile, .email],
@@ -42,18 +50,24 @@ class LoginViewController: UIViewController {
                 print("fb login success")
                 let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
                 Auth.auth().signIn(with: credential) { (authResult, error) in
-                  if let error = error {
-                    print("fb login error: \(error)")
-                    return
-                  }
-                  // User is signed in
-                  print("fb login")
+                    if let error = error {
+                        print("firebase fb login error: \(error)")
+                        return
+                    }
+                    print("firebase fb login")
+//                    if let refreshToken = authResult?.user.refreshToken {
+//                        UserDefaults.standard.set(refreshToken, forKey: "firebaseToken")
+//                    }
                 }
             }
         }
     }
     
-    @IBAction func signOut(_ sender: Any) {
+    @IBAction func signOutPress(_ sender: Any) {
+        signOut()
+    }
+    
+    func signOut() {
         let firebaseAuth = Auth.auth()
         do {
             try firebaseAuth.signOut()
---
+++ Tastopia/UIColor+Extension.swift
@@ -0,0 +1,54 @@
+//
+//  UIColor+Extension.swift
+//  Tastopia
+//
+//  Created by FISH on 2020/1/30.
+//  Copyright © 2020 FISH. All rights reserved.
+//
+
+import UIKit
+
+private enum TTColor: String {
+
+    // swiftlint:disable identifier_name
+    case SUMI
+    
+    case HAI
+}
+
+extension UIColor {
+
+    static let SUMI = TTColor(.SUMI)
+    
+    static let HAI = TTColor(.HAI)
+    
+    // swiftlint:enable identifier_name
+    
+    private static func TTColor(_ color: TTColor) -> UIColor? {
+
+        return UIColor(named: color.rawValue)
+    }
+
+    static func hexStringToUIColor(hex: String) -> UIColor {
+
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if (cString.count) != 6 {
+            return UIColor.HAI!
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
