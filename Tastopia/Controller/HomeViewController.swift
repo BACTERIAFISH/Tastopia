acf80419f4f0b1e8e8120fe34c12568ad0d71911
Bacteriafish <you@example.com>
2020-02-01 10:43:41 +0800

    move files into appropriate groups add ask user location auth add user location in map

--- Tastopia/Controller/HomeViewController.swift
+++ Tastopia/Controller/HomeViewController.swift
@@ -18,14 +18,20 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var taskButton: UIButton!
     @IBOutlet weak var taskViewBottomConstraint: NSLayoutConstraint!
     
+    var locationManager = CLLocationManager()
+    
     override func loadView() {
         super.loadView()
         
         mapView.delegate = self
-        mapView.settings.myLocationButton = true
-
-        let camera = GMSCameraPosition.camera(withLatitude: 25.042461, longitude: 121.564931, zoom: 18.0)
-        mapView.camera = camera
+        let locationAuthStatus = CLLocationManager.authorizationStatus()
+        if locationAuthStatus == .authorizedAlways || locationAuthStatus == .authorizedWhenInUse {
+            mapView.settings.myLocationButton = true
+            mapView.isMyLocationEnabled = true
+        }
+        mapView.settings.compassButton = true
+        
+        mapView.isHidden = true
         
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: 25.042461, longitude: 121.564931)
@@ -34,13 +40,21 @@ class HomeViewController: UIViewController {
         marker.icon = icon
         //marker.snippet = "iOS"
         marker.map = mapView
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        alertLocationAuth()
+        
         taskView.roundCorners(corners: [.topLeft, .topRight], radius: 16)
         taskButton.layer.cornerRadius = 5
+        
     }
     
     @IBAction func signOutPress(_ sender: Any) {
@@ -61,6 +75,17 @@ class HomeViewController: UIViewController {
             
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    func alertLocationAuth() {
+        if CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .restricted {
+            DispatchQueue.main.async {
+                let alertController = UIAlertController(title: "定位權限未開啟", message: "為了有更佳的遊戲體驗，請至 設定 > 隱私權 > 定位服務，變更權限。", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
         }
     }
 }
@@ -84,7 +109,47 @@ extension HomeViewController: GMSMapViewDelegate {
         animator.startAnimation()
     }
     
-    func mapView(_ mapView: GMSMapView, didTapMyLocation location: CLLocationCoordinate2D) {
-        print("222")
+}
+
+extension HomeViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last!
+        
+        let camera = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: 18.0)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        switch status {
+        case .restricted, .denied:
+            print("Location access was restricted or User denied access to location.")
+            let camera = GMSCameraPosition.camera(withLatitude: 25.042529, longitude: 121.564928, zoom: 17.0)
+            mapView.camera = camera
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("Location status is OK.")
+            mapView.settings.myLocationButton = true
+            mapView.isMyLocationEnabled = true
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Location Manager Error: \(error)")
     }
 }
