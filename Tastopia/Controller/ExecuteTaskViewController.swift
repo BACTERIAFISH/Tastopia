7811c6e4be03122ee341e8e99bdd15c5c23df4d4
Bacteriafish <you@example.com>
2020-02-03 18:34:30 +0800

    add date picker in date text field

--- Tastopia/Controller/ExecuteTaskViewController.swift
+++ Tastopia/Controller/ExecuteTaskViewController.swift
@@ -16,10 +16,23 @@ class ExecuteTaskViewController: UIViewController {
     @IBOutlet weak var photoCollectionView: UICollectionView!
     @IBOutlet weak var addPhotoButton: UIButton!
     
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        createDatePicker()
+        dateTextField.inputView = datePicker
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateTextField.text = dateFormatter.string(from: Date())
+        dateTextField.layer.cornerRadius = 5
+        dateTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        dateTextField.leftViewMode = .always
+        
+        compositionTextView.layer.cornerRadius = 16
+        addPhotoButton.layer.cornerRadius = 5
+        
     }
 
     @IBAction func back(_ sender: Any) {
@@ -27,6 +40,21 @@ class ExecuteTaskViewController: UIViewController {
     }
     
     @IBAction func addPhoto(_ sender: UIButton) {
+        
+    }
+    
+    func createDatePicker() {
+        
+        datePicker.datePickerMode = .date
+        datePicker.locale = Locale(identifier: "zh-TW")
+        datePicker.addTarget(self, action: #selector(changeDate), for: .valueChanged)
+    }
+    
+    @objc func changeDate() {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateTextField.text = dateFormatter.string(from: datePicker.date)
     }
     
 }
